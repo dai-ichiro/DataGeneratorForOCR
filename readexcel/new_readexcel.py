130c7183a47317500f67fb2db712654f1d5b67a8
dai-ichiro <d.a.i-ichiro@outlook.jp>
2022-11-01 18:55:06 +0900

    update new_readexcel.py

--- readexcel/new_readexcel.py
+++ readexcel/new_readexcel.py
@@ -2,7 +2,6 @@ import glob
 import pandas as pd
 
 max_len = 35
-split_len = 25
 
 ZEN = "".join(chr(0xff01 + i) for i in range(94))
 HAN = "".join(chr(0x21 + i) for i in range(94))
@@ -10,26 +9,15 @@ HAN2ZEN = str.maketrans(HAN, ZEN)
 
 excel_files = glob.glob('*.xlsx')
 
-general_name = set([])
 product_name = set([])
 
 for excel_file in excel_files:
     df = pd.read_excel(excel_file)
-    general_name = general_name.union(set(df['成分名']))
     product_name = product_name.union(set(df['品名']))
 
-general_name = set([x.replace(' ', '').replace('　', '') for x in general_name])
 product_name = set([x.replace(' ', '').replace('　', '') for x in product_name])
 
-new_general_name = []
 new_product_name = []
-
-for each in general_name:
-    if len(each) > max_len:
-        new_general_name.append(each[:split_len])
-        new_general_name.append(each[-split_len:])
-    else:
-        new_general_name.append(each)
         
 for each in product_name:
     if len(each) < (max_len -3):
@@ -37,14 +25,14 @@ for each in product_name:
             with_header = f'［{str(i).translate(HAN2ZEN)}］{each}'
             new_product_name.append(with_header)
     elif len(each) > max_len:
-        new_product_name.append(each[:split_len])
-        new_product_name.append(each[-split_len:])
+        new_product_name.append(each[:max_len])
+        new_product_name.append(each[-max_len:])
     else:
         new_product_name.append(each)
 
-all_names = set(new_general_name) | set(new_product_name)
+all_names = set(new_product_name)
 
-with open(f'texts_{max_len}_{split_len}.txt', 'w', encoding='utf-8') as f:
+with open(f'texts_max_len_{max_len}.txt', 'w', encoding='utf-8') as f:
     f.write('\n'.join(all_names))
 
 with open('dicts.txt', 'w', encoding='utf-8') as f:
