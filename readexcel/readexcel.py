3e230ae23f56946155481fe3c9e741c3a0e0c86c
dai <d.a.i-ichiro@outlook.jp>
2022-11-02 09:25:55 +0900

    update simple reader

--- readexcel/readexcel.py
+++ readexcel/readexcel.py
@@ -7,9 +7,8 @@ drugs = set([])
 
 for excel_file in excel_files:
     df = pd.read_excel(excel_file)
-    set1 = set(df['成分名'])
-    set2 = set(df['品名'])
-    total = set([x.replace(' ', '').replace('　', '') for x in (set1 | set2)])
+    set1 = set(df['品名'])
+    total = set([x.replace(' ', '').replace('　', '') for x in set1])
     drugs = drugs.union(total)
 
 with open('text.txt', 'w', encoding='utf-8') as f:
